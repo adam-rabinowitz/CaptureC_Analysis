69aaf4bed21452e1e074967a7f11e258fc876840
Adam Rabinowitz <adam.rabinowitz@embl.de>
2018-03-23 17:14:31 +0100

    functions to read input files moved to capturec_input file

--- scripts/differential_analysis.R
+++ scripts/differential_analysis.R
@@ -1,20 +1,20 @@
 # Script to generate distance-decay plots for replicates of all samples
 rm(list=ls())
 require('parallel')
-source('/g/furlong/adamr/github/CaptureC_Analysis/functions/capturec_normalisation.R')
-source('/g/furlong/adamr/github/CaptureC_Analysis/functions/capturec_input.R')
-source('/g/furlong/adamr/github/CaptureC_Analysis/functions/capturec_differential.R')
+source('~/github/CaptureC_Analysis/functions/capturec_normalisation.R')
+source('~/github/CaptureC_Analysis/functions/capturec_input.R')
+source('~/github/CaptureC_Analysis/functions/capturec_differential.R')
 # Set and print parameters
 params <- list(
   'indir' = '/g/furlong/project/37_Capture-C/data/diffinter/input/',
   'pattern' = '_Rep\\dRep\\d.counts.txt$',
-  'outdir' = '/g/furlong/project/37_Capture-C/data/diffinter/results/',
+  'outdir' = '/g/furlong/project/37_Capture-C/data/diffinter/results/fragments/',
   'comparisons' = '/g/furlong/project/37_Capture-C/data/diffinter/comparisons.txt',
   'chrfile' = '/g/furlong/project/37_Capture-C/data/diffinter/chr_sizes.txt',
   'mindist' = 2000,
   'binsize' = 1000,
   'k' = 20,
-  'minsum' = 4,
+  'minsum' = 1,
   'cores' = 8)
 cat('Parameters:\n')
 for (p in names(params)) {
@@ -57,22 +57,19 @@ for (cn in names(comparisons)) {
   cat(paste0('\t', cn, ':\n', '\t\t - ', comparisons[[cn]][1], '\n\t\t - ',
     comparisons[[cn]][2], '\n'))}
 # Create fits
-frequency.fits <- lapply(
+replicate.fits <- lapply(
   input.paths,
   distance.decay.fit,
   min.dist=params$mindist,
   bin.size=params$binsize,
   k=params$k,
   chr.sizes=chr.sizes,
-  cores=params$cores)
-frequency.fits <- do.call(c, frequency.fits)
-# Extract intrachromosomal count data
-input.counts <- lapply(
-  input.paths,
-  read.split.replicates,
-  min.dist=params$mindist,
-  intra.only=T)
-names(input.counts) <- gsub('_Rep1Rep2.counts.txt', '', basename(input.paths))
+  cores=params$cores
+)
+# Read in counts for each condition
+condition.counts <- extract.condition.counts(
+  input.paths
+)
 # Loop through comparisons
 cat('\nStarted:\n')
 for (cmp.name in names(comparisons)) {
@@ -82,9 +79,10 @@ for (cmp.name in names(comparisons)) {
   cond2 <- comparisons[[cmp.name]][2]
   # Merge data
   merged.data <- merge.datasets(
-    input.counts[[cond1]],
-    input.counts[[cond2]],
-    cores=params$cores)
+    condition.counts[[cond1]],
+    condition.counts[[cond2]],
+    cores=params$cores
+  )
   # Perform differential analysis with fit
   deseq.norm.results <- mclapply(
     merged.data,
@@ -101,21 +99,21 @@ for (cmp.name in names(comparisons)) {
     paste0(cmp.name, '.deseq2_norm_results.txt'))
   write.table(
     deseq.norm.results, norm.path, row.names=F, col.names=T, sep='\t', quote=F)
-  # Perform differential analysis without fit
-  deseq.raw.results <- mclapply(
-    merged.data,
-    perform.deseq.analysis,
-    fits=NULL,
-    min.sum=params$minsum,
-    mc.cores=params$cores)
-  # Merge data and recalculate p-value
-  deseq.raw.results <- rbindlist(deseq.raw.results)
-  deseq.raw.results$padj <- p.adjust(deseq.raw.results$pvalue, method='fdr')
-  # Save file
-  raw.path <- file.path(
-    params$outdir,
-    paste0(cmp.name, '.deseq2_raw_results.txt'))
-  write.table(
-    deseq.raw.results, raw.path, row.names=F, col.names=T, sep='\t', quote=F)
+  # # Perform differential analysis without fit
+  # deseq.raw.results <- mclapply(
+  #   merged.data,
+  #   perform.deseq.analysis,
+  #   fits=NULL,
+  #   min.sum=params$minsum,
+  #   mc.cores=params$cores)
+  # # Merge data and recalculate p-value
+  # deseq.raw.results <- rbindlist(deseq.raw.results)
+  # deseq.raw.results$padj <- p.adjust(deseq.raw.results$pvalue, method='fdr')
+  # # Save file
+  # raw.path <- file.path(
+  #   params$outdir,
+  #   paste0(cmp.name, '.deseq2_raw_results.txt'))
+  # write.table(
+  #   deseq.raw.results, raw.path, row.names=F, col.names=T, sep='\t', quote=F)
 }
 
