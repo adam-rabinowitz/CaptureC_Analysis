9cad1354ff6521e8122d4e8689e93edf9ad37f74
Adam Rabinowitz <adam.rabinowitz@embl.de>
2018-03-28 14:32:46 +0200

    added to purrr::transpose function to manipulate list of lists

--- functions/capturec_input.R
+++ functions/capturec_input.R
@@ -1,55 +1,36 @@
 require('data.table')
-require('plyr')
+require('purrr')
 
-# ##############################################################################
-# ## Function to replace NA values in one vector with values from other vector
-# ##############################################################################
-# fillNA <- function(v1, v2) {
-#   indices <- is.na(v1)
-#   v1[indices] <- v2[indices]
-#   return(v1)
-# }
-# 
-# ##############################################################################
-# ## Function to replace NA values in one vector with zero
-# ##############################################################################
-# zeroNA <- function(v) {
-#   indices <- is.na(v)
-#   v[indices] <- 0
-#   return(v)
-# }
-
-# ##############################################################################
-# ## Function to merge view points
-# ##############################################################################
-# merge.bait.data <- function(x1, x2) {
-#   # Extract data and merge
-#   cond1 <- x1$dataset[1]
-#   cond2 <- x2$dataset[1]
-#   merged <- merge(x=x1, y=x2, by='fragID', all=T)
-#   # Create output
-#   merged.df <- data.frame(
-#     'cond1' = cond1,
-#     'cond2' = cond2,
-#     'baitName' = fillNA(merged$baitName.x, merged$baitName.y),
-#     'baitID' = fillNA(merged$baitID.x, merged$baitID.y),
-#     'baitChr' = fillNA(merged$baitChr.x, merged$baitChr.y),
-#     'baitStart' = fillNA(merged$baitStart.x, merged$baitStart.y),
-#     'baitEnd' = fillNA(merged$baitEnd.x, merged$baitEnd.y),
-#     'fragID' = merged$fragID,
-#     'fragChr' = fillNA(merged$fragChr.x, merged$fragChr.y),
-#     'fragStart' = fillNA(merged$fragStart.x, merged$fragStart.y),
-#     'fragEnd' = fillNA(merged$fragEnd.x, merged$fragEnd.y),
-#     'fragDist' = fillNA(merged$fragDist.x, merged$fragDist.y),
-#     'cond1.1' = zeroNA(merged$N1.x),
-#     'cond1.2' = zeroNA(merged$N2.x),
-#     'cond2.1' = zeroNA(merged$N1.y),
-#     'cond2.2' = zeroNA(merged$N2.y),
-#     stringsAsFactors=F)
-#   # Order and return output
-#   merged.df <- merged.df[order(merged.df$baitID, merged.df$fragID),]
-#   return(merged.df)
-# }
+###############################################################################
+## function to extract comparisons
+###############################################################################
+extract.comparisons <- function(
+  path
+) {
+  # Read in file
+  comparisons <- read.table(
+    params$comparisons, sep='\t', stringsAsFactors=F
+  )
+  # check comparisons
+  if (
+    any(
+      substring(comparisons[,1], 1, 3) !=
+      substring(comparisons[,2], 1, 3))
+  ) {
+    stop('unmatched pulldowns')
+  }
+  # Extract names
+  cmp.names <- paste(
+    substring(comparisons[,1], 1, 3),
+    substring(comparisons[,1], 5),
+    substring(comparisons[,2], 5),
+    sep='.'
+  )
+  # Split comparsions by name and return data
+  comparisons <- split(comparisons, cmp.names)
+  comparisons <- lapply(comparisons, as.character)
+  return(comparisons)
+}
 
 ###############################################################################
 ## Function to calculate distances
@@ -96,21 +77,35 @@ read.split.replicates <- function(path, min.dist, intra.only) {
 ##############################################################################
 ## Function to merge replicates of a single bait
 ##############################################################################
-merge.replicates.bait <- function(x1, x2) {
+merge.replicates.bait <- function(x) {
+  # Check input
+  if (class(x) != 'list') {
+    stop('input must be a list')
+  }
+  if (length(x) != 2) {
+    stop('list must have a length of two')
+  }
+  if (any(sapply(x, class) != 'data.frame')) {
+    stop('input must be a list of data.frame')
+  }
   # Extract conditions and check identity
-  cond <- unique(c(unique(x1$dataset), c(unique(x2$dataset))))
+  cond <- unique(c(unique(x[[1]]$dataset), c(unique(x[[2]]$dataset))))
   if (length(cond) != 1) {
     stop('multiple conditions found')
   }
   # Check replicates are different
-  rep1 <- colnames(x1)[grepl('^Rep\\d$', colnames(x1))]
-  rep2 <- colnames(x2)[grepl('^Rep\\d$', colnames(x2))]
+  rep1 <- colnames(x[[1]])[grepl('^Rep\\d$', colnames(x[[1]]))]
+  rep2 <- colnames(x[[2]])[grepl('^Rep\\d$', colnames(x[[2]]))]
   reps <- c(rep1, rep2)
   if (length(reps) != length(unique(reps))) {
     stop('overlapping replicates found')
   }
-  # Merge data
-  merged <- merge(x=x1, y=x2, all=T)
+  # Merge data and check all fragments are unique 
+  merged <- merge(x=x[[1]], y=x[[2]], all=T)
+  if (length(merged$fragID) != length(unique(merged$fragID))) {
+    stop('Non-unique fragments present in output data')
+  }
+  # Fill in missing data
   for (rep in reps) {
     rep.counts <- merged[,rep,drop=T]
     rep.counts[is.na(rep.counts)] <- 0
@@ -123,21 +118,116 @@ merge.replicates.bait <- function(x1, x2) {
 ##############################################################################
 ## Function to merge replicates of a single bait
 ##############################################################################
-merge.replicates.all <- function(x1, x2) {
-  # Extract names of all baits
-  baits <- union(names(x1), names(x2))
-  # Transpose baits
-  bait.list <- list()
-  for (b in baits) {
-    bait.list[[b]] <- merge.replicates.bait(x1[[b]], x2[[b]])
+merge.replicates.all <- function(x, cores) {
+  # Check input
+  if (class(x) != 'list') {
+    stop('input must be a list')
   }
+  if (length(x) != 2) {
+    stop('list must have a length of two')
+  }
+  # Check names of all baits
+  if (!all(names(x[[1]]) == names(x[[2]]))) {
+    stop('different baits present')
+  }
+  # Transpose baits
+  tran.baits <- purrr::transpose(x)
+  bait.list <- parallel::mclapply(
+    tran.baits,
+    merge.replicates.bait,
+    mc.cores=cores
+  )
+  # Return data
   return(bait.list)
 }
 
 ##############################################################################
+## Function to merge view points
+##############################################################################
+merge.conditions.bait <- function(x) {
+  # Check input
+  if (class(x) != 'list') {
+    stop('input must be a list')
+  }
+  if (length(x) != 2) {
+    stop('list must have a length of two')
+  }
+  if (any(sapply(x, class) != 'data.frame')) {
+    stop('input must be a list of data.frame')
+  }
+  # Extract conditions and check identity
+  cond1 <- unique(x[[1]]$dataset)
+  cond2 <- unique(x[[2]]$dataset)
+  if (length(cond1) != 1 | length(cond2) != 1) {
+    stop('multiple conditions found')
+  }
+  if (cond1 == cond2) {
+    stop('conditions are not unique')
+  }
+  # Rename columns for first condition
+  colnames(x[[1]]) <- gsub('^dataset$', 'cond1', colnames(x[[1]]))
+  x1.replicates <- colnames(x[[1]])[grepl('Rep\\d', colnames(x[[1]]))]
+  x1.replicates <- paste(cond1, x1.replicates, sep='.')
+  colnames(x[[1]])[grepl('Rep\\d', colnames(x[[1]]))] <- x1.replicates
+  # Rename columns for second condition
+  colnames(x[[2]]) <- gsub('^dataset$', 'cond2', colnames(x[[2]]))
+  x2.replicates <- colnames(x[[2]])[grepl('Rep\\d', colnames(x[[2]]))]
+  x2.replicates <- paste(cond2, x2.replicates, sep='.')
+  colnames(x[[2]])[grepl('Rep\\d', colnames(x[[2]]))] <- x2.replicates
+  # Merge data and check all entries are unique
+  merged <- merge(x=x[[1]], y=x[[2]], all=T)
+  if (length(merged$fragID) != length(unique(merged$fragID))) {
+    stop('Non-unique fragments present in output data')
+  }
+  # Fill in missing data
+  merged$cond1 <- cond1
+  merged$cond2 <- cond2
+  reps <- colnames(merged)[grepl('\\.Rep\\d+$', colnames(merged))]
+  for (rep in reps) {
+    rep.counts <- merged[,rep,drop=T]
+    rep.counts[is.na(rep.counts)] <- 0
+    merged[,rep] <- rep.counts
+  }
+  # Rorder columns and return
+  merged <- cbind(
+    merged[,c('cond1', 'cond2')],
+    merged[,!grepl('^cond\\d+$', colnames(merged))]
+  )
+  return(merged)
+}
+
+###############################################################################
+## Function to merge all baits in dataset
+###############################################################################
+merge.conditions.all <- function(x, cores) {
+  # Check input
+  if (class(x) != 'list') {
+    stop('input must be a list')
+  }
+  if (length(x) != 2) {
+    stop('list must have a length of two')
+  }
+  # Check names of all baits
+  if (!all(names(x[[1]]) == names(x[[2]]))) {
+    stop('different baits present')
+  }
+  # Transpose baits
+  tran.baits <- purrr::transpose(x)
+  bait.list <- parallel::mclapply(
+    tran.baits,
+    merge.conditions.bait,
+    mc.cores=cores
+  )
+  # Return data
+  return(bait.list)
+}
+  
+##############################################################################
 ## Read counts for files and merge by condition
 ##############################################################################
-extract.condition.counts <- function(input.paths, suffix) {
+extract.condition.counts <- function(
+  input.paths, suffix, min.dist, intra.only, cores
+) {
   # Split paths by condition
   condition.paths <- split(
     input.paths,
@@ -147,20 +237,19 @@ extract.condition.counts <- function(input.paths, suffix) {
   condition.counts <- lapply(
     condition.paths,
     function(paths) {
-      print(paths)
       # Read in count data
       counts <- lapply(
         paths, 
         read.split.replicates,
-        min.dist=params$mindist,
-        intra.only=T
+        min.dist=min.dist,
+        intra.only=intra.only
       )
       # Merge counts if required
       if (length(counts) == 1) {
         counts <- counts[[1]]
       } else if (length(counts) == 2) {
         counts <- merge.replicates.all(
-          counts[[1]], counts[[2]]
+          counts, cores=cores
         )
       } else {
         stop('more than two replicates not considered')
@@ -169,64 +258,7 @@ extract.condition.counts <- function(input.paths, suffix) {
       return(counts)
     }
   )
+  # Rename condition counts and return
+  return(condition.counts)
 }
-
-##############################################################################
-## Function to merge view points
-##############################################################################
-merge.conditions.bait <- function(x1, x2) {
-  # Extract conditions and check identity
-  cond1 <- unique(x1$dataset)
-  cond2 <- unique(x2$dataset)
-  if (length(cond1) != 1 | length(cond2) != 1) {
-    stop('multiple conditions found')
-  }
-  if (cond1 == cond2) {
-    stop('conditions are not unique')
-  }
-  # Rename columns
-  x1 <- plyr::rename(x1, replace=c('dataset'='cond1'))
-  x1.replicates <- colnames(x1)[grepl('Rep\\d', colnames(x1))]
-  x1.replicates <- paste(cond1, x1.replicates, sep='.')
-  colnames(x1)[grepl('Rep\\d', colnames(x1))] <- x1.replicates
-  x2 <- plyr::rename(x2, replace=c('dataset'='cond2'))
-  x2.replicates <- colnames(x2)[grepl('Rep\\d', colnames(x2))]
-  x2.replicates <- paste(cond2, x2.replicates, sep='.')
-  colnames(x2)[grepl('Rep\\d', colnames(x2))] <- x2.replicates
-  # Merge data
-  merged <- merge(x=x1, y=x2, all=T)
-  # Fill in missing data
-  merged$cond1 <- cond1
-  merged$cond2 <- cond2
-  reps <- colnames(merged)[grepl('\\.Rep\\d', colnames(merged))]
-  for (rep in reps) {
-    rep.counts <- merged[,rep,drop=T]
-    rep.counts[is.na(rep.counts)] <- 0
-    merged[,rep] <- rep.counts
-  }
-  # Rorder columns and return
-  merged <- cbind(
-    merged[,c('cond1', 'cond2')],
-    merged[,!grepl('cond\\d', colnames(merged))]
-  )
-  return(merged)
-}
-
-###############################################################################
-## Function to merge all baits in dataset
-###############################################################################
-merge.datasets <- function(d1, d2, cores) {
-  # Find common baits
-  baits <- union(names(d1), names(d2))
-  # Merge data for each bait
-  merged.data <- mclapply(
-    baits,
-    function(bait) {
-      merge.bait.data(d1[[bait]], d2[[bait]])},
-    mc.cores=cores)
-  return(merged.data)
-}
-  
-
-
 
