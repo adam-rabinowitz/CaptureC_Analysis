f57394e518d4953385a6d90f4bef9b050cdb7146
Adam Rabinowitz <adam.rabinowitz@embl.de>
2018-03-28 14:36:03 +0200

    Altered script to test three alternative hypotheses

--- scripts/differential_analysis.R
+++ scripts/differential_analysis.R
@@ -6,24 +6,50 @@ source('~/github/CaptureC_Analysis/functions/capturec_input.R')
 source('~/github/CaptureC_Analysis/functions/capturec_differential.R')
 # Set and print parameters
 params <- list(
-  'indir' = '/g/furlong/project/37_Capture-C/data/diffinter/input/',
-  'pattern' = '_Rep\\dRep\\d.counts.txt$',
-  'outdir' = '/g/furlong/project/37_Capture-C/data/diffinter/results/fragments/',
+  'indir' = '/g/furlong/project/37_Capture-C/data/diffinter/input',
+  'pattern' = '_Rep\\d+Rep\\d+.counts.txt$',
+  'outdir' = '/g/furlong/project/37_Capture-C/data/diffinter/all_replicate_results/fragments',
   'comparisons' = '/g/furlong/project/37_Capture-C/data/diffinter/comparisons.txt',
   'chrfile' = '/g/furlong/project/37_Capture-C/data/diffinter/chr_sizes.txt',
   'mindist' = 2000,
   'binsize' = 1000,
   'k' = 20,
-  'minsum' = 1,
-  'cores' = 8)
+  'minmean' = 1,
+  'cores' = 8
+)
 cat('Parameters:\n')
 for (p in names(params)) {
   cat(paste0('\t', p, ' : ', params[[p]], '\n'))}
+# Set and print alternative hypotheses for deseq
+alt.hypotheses <- list(
+  'above0.0' = list(
+    'lfcThreshold' = 0,
+    'altHypothesis' = 'greaterAbs'
+  ),
+  'above1.0' = list(
+    'lfcThreshold' = 1,
+    'altHypothesis' = 'greaterAbs'
+  ),
+  'below0.5' = list(
+    'lfcThreshold' = 0.5,
+    'altHypothesis' = 'lessAbs'
+  )
+)
+cat('\nAltHypotheses:\n')
+for (a in names(alt.hypotheses)) {
+  cat(
+    paste0(
+      '\t', a, ':\n\t\tlfcThreshold : ', alt.hypotheses[[a]]$lfcThreshold,
+      '\n\t\taltHypothesis : ', alt.hypotheses[[a]]$altHypothesis, '\n'
+    )
+  )
+}
 # Extract input file paths and print
 input.paths <- list.files(
   params$indir,
   pattern=params$pattern,
-  full.names=T)
+  full.names=T
+)
 cat('\nInput Paths:\n')
 for (ip in input.paths) {
   cat(paste0('\t - ', ip, '\n'))}
@@ -35,40 +61,28 @@ cat('\nChromosome Sizes:\n')
 for (chr in names(chr.sizes)) {
   cat(paste0('\t', chr, ' : ', chr.sizes[chr], '\n'))}
 # Extract comparisons
-comparisons <- read.table(
-  params$comparisons, sep='\t', stringsAsFactors=F)
-# check comparisons and get names
-if (
-  any(
-    substring(comparisons[,1], 1, 3) !=
-    substring(comparisons[,2], 1, 3))
-) {
-  stop('unmatched pulldowns')}
-cmp.names <- paste(
-  substring(comparisons[,1], 1, 3),
-  substring(comparisons[,1], 5),
-  substring(comparisons[,2], 5),
-  sep='.')
-# Split comparsions
-comparisons <- split(comparisons, cmp.names)
-comparisons <- lapply(comparisons, as.character)
+comparisons <- extract.comparisons(params$comparisons)
 cat('\nComparisons:\n')
 for (cn in names(comparisons)) {
-  cat(paste0('\t', cn, ':\n', '\t\t - ', comparisons[[cn]][1], '\n\t\t - ',
-    comparisons[[cn]][2], '\n'))}
+  cat(
+    paste0(
+      '\t', cn, ':\n', '\t\t - ', comparisons[[cn]][1], '\n\t\t - ',
+      comparisons[[cn]][2], '\n'
+    )
+  )
+}
 # Create fits
-replicate.fits <- lapply(
+distance.fits <- distance.decay.fit.all(
   input.paths,
-  distance.decay.fit,
   min.dist=params$mindist,
   bin.size=params$binsize,
   k=params$k,
-  chr.sizes=chr.sizes,
-  cores=params$cores
+  chr.sizes=chr.sizes
 )
 # Read in counts for each condition
 condition.counts <- extract.condition.counts(
-  input.paths
+  input.paths, suffix=params$pattern, min.dist=params$mindist, intra.only=T,
+  cores=params$cores
 )
 # Loop through comparisons
 cat('\nStarted:\n')
@@ -78,42 +92,49 @@ for (cmp.name in names(comparisons)) {
   cond1 <- comparisons[[cmp.name]][1]
   cond2 <- comparisons[[cmp.name]][2]
   # Merge data
-  merged.data <- merge.datasets(
-    condition.counts[[cond1]],
-    condition.counts[[cond2]],
+  merged.data <- merge.conditions.all(
+    condition.counts[c(cond1, cond2)],
     cores=params$cores
   )
-  # Perform differential analysis with fit
-  deseq.norm.results <- mclapply(
-    merged.data,
-    perform.deseq.analysis,
-    fits=frequency.fits,
-    min.sum=params$minsum,
-    mc.cores=params$cores)
-  # Merge data and recalculate p-value
-  deseq.norm.results <- rbindlist(deseq.norm.results)
-  deseq.norm.results$padj <- p.adjust(deseq.norm.results$pvalue, method='fdr')
+  # Perform deseq analysis
+  norm.results <- deseq.analysis.all(
+    bait.list=merged.data,
+    fits=distance.fits,
+    min.mean=1,
+    alt.hypotheses=alt.hypotheses,
+    cores=1
+  )
   # Save file
-  norm.path <- file.path(
-    params$outdir,
-    paste0(cmp.name, '.deseq2_norm_results.txt'))
-  write.table(
-    deseq.norm.results, norm.path, row.names=F, col.names=T, sep='\t', quote=F)
-  # # Perform differential analysis without fit
-  # deseq.raw.results <- mclapply(
-  #   merged.data,
-  #   perform.deseq.analysis,
-  #   fits=NULL,
-  #   min.sum=params$minsum,
-  #   mc.cores=params$cores)
-  # # Merge data and recalculate p-value
-  # deseq.raw.results <- rbindlist(deseq.raw.results)
-  # deseq.raw.results$padj <- p.adjust(deseq.raw.results$pvalue, method='fdr')
-  # # Save file
-  # raw.path <- file.path(
-  #   params$outdir,
-  #   paste0(cmp.name, '.deseq2_raw_results.txt'))
-  # write.table(
-  #   deseq.raw.results, raw.path, row.names=F, col.names=T, sep='\t', quote=F)
+  for (ah in names(alt.hypotheses)) {
+    # Create output path
+    norm.path <- file.path(
+      params$outdir,
+      paste(cmp.name, ah, 'deseq2_norm_results.txt', sep='.')
+    )
+    # Write data to file
+    write.table(
+      norm.results[[ah]], norm.path, row.names=F, col.names=T, sep='\t', quote=F 
+    )
+  }
+  # Merge data and recalculate p-value
+  raw.results <- deseq.analysis.all(
+    bait.list=merged.data,
+    fits=NULL,
+    min.mean=params$minmean,
+    alt.hypotheses=alt.hypotheses,
+    cores=1
+  )
+  # Save raw results for each hypothesis
+  for (ah in names(alt.hypotheses)) {
+    # Create output path
+    raw.path <- file.path(
+      params$outdir,
+      paste(cmp.name, ah, 'deseq2_raw_results.txt', sep='.')
+    )
+    # Write data to file
+    write.table(
+      raw.results[[ah]], raw.path, row.names=F, col.names=T, sep='\t', quote=F 
+    )
+  }
 }
 
